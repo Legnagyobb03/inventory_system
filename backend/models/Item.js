c0cafc527b8e446a274b1740afee6052f0c65c83
Horváth Alex Kristóf <horvathhalex16@gmail.com>
2024-12-22 22:13:26 +0100

    database: Add createdBy and createdAt to the Item model.

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -2,9 +2,16 @@ const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String},
-  quantity: { type: Number, required: true },
-  location: {type: String, enum:["Section A", "Section B", "Section C", "Section D"], default:"Undefined"}
+  description: { type: String },
+  quantity: { type: Number, default: 0 },
+  location: {
+    type: String,
+    enum: ["Section A", "Section B", "Section C", "Section D", "Undefined"],
+    default: "Undefined",
+  },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Item', itemSchema);
+
